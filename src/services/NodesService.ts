68616b46867e48e25ff9e6d653c60193c9061235
el-davo <david.ahern@emc.com>
2019-03-24 16:14:57 +0000

    Adding retry to test results

--- src/services/NodesService.ts
+++ src/services/NodesService.ts
@@ -1,8 +1,13 @@
 import { post, patch } from 'superagent';
 import { inject } from '@loopback/context';
 import { Node } from '../models/node.model';
+import { retry } from 'async';
+import { Logger } from 'winston';
 
 export class NodesService {
+
+    @inject('logger')
+    private logger: Logger;
 
     @inject('api.jobs')
     private jobsApi: string;
@@ -18,12 +23,18 @@ export class NodesService {
     }
 
     async saveTestResults(node: Node, successful: number, failed: number, dropped: number) {
-        await patch(`${this.jobsApi}/nodes/${node.id}`).send({
-            ...node,
-            running: false,
-            totalSuccessfulConnections: successful,
-            totalFailedConnections: failed,
-            totalDroppedConnections: dropped
+        await new Promise((resolve) => {
+            retry({times: 200, interval: 5000}, done => {
+                this.logger.info('Recording test results');
+
+                patch(`${this.jobsApi}/nodes/${node.id}`).send({
+                    ...node,
+                    running: false,
+                    totalSuccessfulConnections: successful,
+                    totalFailedConnections: failed,
+                    totalDroppedConnections: dropped
+                }).then(() => done());
+            }, () => resolve());
         });
     }
 }
